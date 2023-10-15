6764ab0b99f6bc957590289c11f3a4fc9393e052
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-15 09:48:34 -0700

    Print size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+/* Calculates and prints sizes of data types in bytes */
 int main(void)
 {
 /* Print the sizeof a char */
-printf("Sizeofachar:%ldbyte(s)\n", sizeof(char));
+printf("Size of a char: %ldbyte(s)\n", sizeof(char));
 /* Print the sizeof an int */
-printf("Sizeofanint:%ldbyte(s)\n", sizeof(int));
+printf("Size of an int: %ldbyte(s)\n", sizeof(int));
 /* Print the sizeof a long int */
-printf("Sizeofalongint:%ldbyte(s)\n", sizeof(long));
+printf("Size of along int: %ldbyte(s)\n", sizeof(long));
 /* Print the sizeof a long long int */
-printf("Sizeofalonglongint:%ldbyte(s)\n", sizeof(long long));
+printf("Size of a long long int: %ldbyte(s)\n", sizeof(long long));
 /* Print the sizeof a float */
-printf("Sizeofafloat:%ldbyte(s)\n", sizeof(float));
+printf("Size of a float: %ldbyte(s)\n", sizeof(float));
 return (0);
 }
 
