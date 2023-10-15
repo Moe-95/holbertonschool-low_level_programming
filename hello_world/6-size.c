8f3110a959b14b4ebbdde41f658d538a578254e8
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-15 09:43:49 -0700

    Done with project 1 c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+/* Print the sizeof a char */
+printf("Sizeofachar:%ldbyte(s)\n", sizeof(char));
+/* Print the sizeof an int */
+printf("Sizeofanint:%ldbyte(s)\n", sizeof(int));
+/* Print the sizeof a long int */
+printf("Sizeofalongint:%ldbyte(s)\n", sizeof(long));
+/* Print the sizeof a long long int */
+printf("Sizeofalonglongint:%ldbyte(s)\n", sizeof(long long));
+/* Print the sizeof a float */
+printf("Sizeofafloat:%ldbyte(s)\n", sizeof(float));
+return (0);
+}
+
