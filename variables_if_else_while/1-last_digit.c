920a6a952f8895d558c3b113b09c0a6300f73c29
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-19 14:17:03 -0700

    Less or equal

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,22 +1,21 @@
 #include <stdlib.h>
-#include <stdio.h>  
+#include <stdio.h>
 #include <time.h>
 /**
-*main - This funtion creates a random number 
+*main - This funtion creates a random number
 *and print the last digit of it.
-*Return: Always 0. 
+*Return: Always 0.
 */
-
-int main(void) 
-{  
+int main(void)
+{
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n%10 > 5)
-printf("Last digit of %d is %d and is greater than 5", n, n%10);   
-if (n%10 == 0)
-printf("Last digit of %d is %d and is 0", n, n%10);
-if (n%10 < 6 && n%10 != 0)
-printf("Last digit of %d is %d and is less than 6 and not 0", n, n%10);
-return (0); 
+if (n % 10 > 5)
+printf("Last digit of %d is %d and is greater than 5", n, n % 10);
+if (n % 10 == 0)
+printf("Last digit of %d is %d and is 0", n, n % 10);
+if (n % 10 < 6 && n % 10 != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
+return (0);
 }
