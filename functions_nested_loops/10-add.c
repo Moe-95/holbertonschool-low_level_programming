310eb61516e9947bc192a65272b38e6db8d60d47
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-21 12:32:02 -0700

    Summation

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - Calculate the sum of 2 numbers.
+*@a: number to be added
+*@b: number to be added 
+*Return: sum of a b.
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
