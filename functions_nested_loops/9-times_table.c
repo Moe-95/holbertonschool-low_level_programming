95e50a6942a6846bd376750f4bbc871b307586b4
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-21 12:25:22 -0700

    Table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,19 +4,19 @@
 */
 void times_table(void)
 {
-int i = 0, j = 0;  
+int i = 0, j = 0;
 while (i < 10)
 {
-j = 0;    
+j = 0;
 while (j < 10)
 {
-int a = j * i;
+int a = j *i;
 if (j == 0)
-{	
-_putchar('0' + a);	
+{
+_putchar('0' + a);
 }
 else
-{	
+{
 _putchar(',');
 _putchar(' ');
 if (a < 10)
@@ -26,7 +26,7 @@ _putchar('0' + a);
 }
 else
 {
-_putchar((a / 10) + '0'); 
+_putchar((a / 10) + '0');
 _putchar((a % 10) + '0');
 }
 }
