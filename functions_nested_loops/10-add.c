392cded7422fa47d2747634d5732067aabcc77df
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-21 12:51:22 -0700

    Print to 98

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
 * add - Calculate the sum of 2 numbers.
 *@a: number to be added
-*@b: number to be added 
+*@b: number to be added
 *Return: sum of a b.
 */
 int add(int a, int b)
---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*print_to_98 - Display numbers till 98.
+*@n: number to be checked.
+*
+*/
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+while (n < 99)
+{
+if (n < 98)
+{
+printf("%d", n);
+printf(",");
+printf(" ");
+}
+else
+printf("%d", n);
+n++;
+}
+}
+else
+{
+while (n > 97)
+{
+if (n > 98)
+{
+printf("%d", n);
+printf(",");
+printf(" ");
+}
+else
+printf("%d", n);
+n--;
+}
+}
+}
