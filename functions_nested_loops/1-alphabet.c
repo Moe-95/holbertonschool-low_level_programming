9c23f20f2fffdc265c2ac70b74c7d7b393e8ec1c
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-21 08:36:35 -0700

    Alpha

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main- Print the alphabet.
+*
+*Return: ALways 0.
+*/
+void print_alphabet(void)
+{
+int i = 97;
+while (i < 123)
+{
+_putchar(i);
+if (i == 122)
+_putchar('\n');
+i++;
+}
+}
