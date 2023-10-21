346b8b8d97c90e2cca5ea453803f493902dd7c97
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-21 10:17:19 -0700

    Modulo 10

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_last_digit - Checks the last digit of a number.
+*@c: number to be checked.
+*
+*Return: last digit of c.
+*/
+int print_last_digit(int c)
+{
+return (c % 10);
+}
