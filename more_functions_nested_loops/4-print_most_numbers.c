481c0ab54d47a576bd5fca322f3f4405eeefabfa
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-22 05:41:05 -0700

    Print numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_most_numbers - Print from 0 to 9 without 2 and 4.
+*
+*/
+void print_most_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+if (i == 2 || i == 4)
+i++;
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
