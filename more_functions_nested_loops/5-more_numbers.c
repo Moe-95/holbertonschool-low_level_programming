51776b59461651c24754662a11e0ea4798ebd6aa
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-22 06:14:01 -0700

    10 times decimal

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*more_numbers - Print from 0 to 14 10times.
+*
+*/
+void more_numbers(void)
+{
+int i = 0, j = 0;
+while (j < 10)
+{
+while (i < 15)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+j++;
+}
+}
