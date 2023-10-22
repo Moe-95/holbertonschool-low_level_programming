431191195bcdb45aaf4cfc17ecdc49a569aa6c82
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-22 05:25:33 -0700

    More functions

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul - Check if is digit.
+*@a: the number to multiply by.
+*@b: the number to multiply with.
+*Return: a multiply by b.
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
