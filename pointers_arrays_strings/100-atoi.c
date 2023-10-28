67a043a22875db6292ee32a024efdca8d801d4df
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 11:45:15 -0700

    String to nber

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -15,16 +15,15 @@ sign = sign * (-1);
 while (s[i] >= '0' && s[i] <= '9')
 {
 dig = 1;
-num = num * 10 + (s[i] - '0');
+if (sign == -1)
+num = num * 10 - (s[i] - '0');
+else
+num = num * 10 + (s[i] - '0');   
 i++;
 }
 if (dig == 1)
 break;
 i++;
 }
-if (sign == -1)
-num = -num;
-if (num < INT_MIN)
-num = INT_MIN;
 return (num);
 }
