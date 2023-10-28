4c49349735721ee349f24e42b8e8f4ca7e598bf2
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 12:48:06 -0700

    Concatenation

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*_strcat - Concatenate a String.
+*
+*@src: Pointer to str.
+*@dest: Pointer to destination string.
+*
+*Return: Concatenated String.
+*/
+char *_strcat(char *dest, char *src)
+{
+int i = 0, j = 0, k = 0, q = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+j++;
+}
+while (i < k)
+{
+dest[i] = src[q];
+i++;
+q++;
+}
+return (dest);
+}
