104513fe1873867c48e24a95c0351f97404262c6
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 10:54:11 -0700

    String to number

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,15 +7,21 @@
 */
 int _atoi(char *s)
 {
-int i = 0, sign = 1, num = 0;
+int i = 0, sign = 1, num = 0, dig = 0;
 while (s[i] != '\0')
 {
 if (s[i] == '-')
 sign = sign * (-1);
 else if (s[i] == '+')
 sign = sign * (+1);
-if (s[i] >= '0' && s[i] <= '9')
+while (s[i] >= '0' && s[i] <= '9')
+{
+dig = 1;
 num = num * 10 + (s[i] - '0');
+i++;
+}
+if (dig == 1)
+break;
 i++;
 }
 return (num *sign);
