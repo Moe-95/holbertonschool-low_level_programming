ee627296dad9d2304ad10c3271410b5376b829aa
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 07:57:08 -0700

    Print array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,10 @@ void print_array(int *a, int n)
 int i = 0;
 while (i < n)
 {
-if (i == (n - 1))
-printf("%d\n", a[i]);
-else
-printf("%d, ", a[i]);
+printf("%d", a[i]);
+if (i < (n - 1))
+printf(", ");
 i++;
 }
+printf("\n");
 }
