ae6c706a947ab67997f50fd5362c4be5d85dbb84
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 13:42:33 -0700

    Concate

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_strncpy - Concatenate a String.
+*
+*@src: Pointer to str.
+*@dest: Pointer to destination string.
+*@n: Number of chars to concatenate.
+*
+*Return: Concatenated String.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
