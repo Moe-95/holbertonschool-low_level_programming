da66aac6cd812a7e714d3d3c1762650c0260f2ed
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 10:46:02 -0700

    String to nb

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,9 +11,9 @@ int i = 0, sign = 1, num = 0;
 while (s[i] != '\0')
 {
 if (s[i] == '-')
-sign = sign * (-1);
+num = num * (-1);
 else if (s[i] == '+')
-sign = sign * (+1);
+num = num * (+1);
 if (s[i] >= '0' && s[i] <= '9')
 num = num * 10 + (s[i] - '0');
 i++;
