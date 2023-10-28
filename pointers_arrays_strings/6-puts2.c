5e6d459575486418d49d50e461cba2aca5b623a7
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 06:49:08 -0700

    Other char

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*puts2 - Prints other character of a String.
+*
+*@str: Pointer to str.
+*/
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i++]);
+i++;
+}
+_putchar('\n');
+}
