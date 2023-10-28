d1f487ebe996f48d9b346298688d1e9ef86e5508
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 10:47:21 -0700

    String to nber

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,9 +11,9 @@ int i = 0, sign = 1, num = 0;
 while (s[i] != '\0')
 {
 if (s[i] == '-')
-num = num * (-1);
+sign = sign * (-1);
 else if (s[i] == '+')
-num = num * (+1);
+sign = sign * (+1);
 if (s[i] >= '0' && s[i] <= '9')
 num = num * 10 + (s[i] - '0');
 i++;
