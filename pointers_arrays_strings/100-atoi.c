16614b590f7e68cf9bf8a7590870c9780979247e
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 10:14:50 -0700

    Numbers from string

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_atoi- Get numbers from a String.
+*
+*@s: String pointed.
+*Return: Signed number.
+*/
+int _atoi(char *s)
+{
+int i = 0, sign = 1, num = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+sign = sign * (-1);
+else if (s[i] == '+')
+sign = sign * (+1);
+if (s[i] >= '0' && s[i] <= '9')
+num = num * 10 + (s[i] - '0');
+i++;
+}
+return (num *sign);
+}
