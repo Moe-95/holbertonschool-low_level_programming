deaed77d00a5fa7f1f841dff955d2a4cf5b55b38
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 20:03:40 -0700

    Compare 2 strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,10 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-  int i = 0, cmp = 0;
+int i = 0, cmp = 0;
 while (s1[i] != '\0' && s2[i] != '\0')
 {
-if (s1[i] == s2[i])
-i++;
-else
+if (s1[i] != s2[i])
 {
 cmp += (s1[i] - s2[i]);
 break;
