e0cd3b249b777c02e673f3d6f237895280972be0
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-28 19:51:39 -0700

    Compare 2 strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_strcmp - Compare 2 strings.
+*
+*@s1: Pointer to s1.
+*@s2: Pointer to s2.
+*Return: difference between strs cmp.
+*/
+int _strcmp(char *s1, char *s2);
+{
+int i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] == s2[i])
+i++;
+else
+{
+cmp += (s1[i] - s2[i]);
+break;
+}
+i++;
+}
+return (cmp);
+}
