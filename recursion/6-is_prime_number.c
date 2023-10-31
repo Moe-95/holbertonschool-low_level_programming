f0c30769ca985da9265d62125bf3a7d732948e8a
Moe-95 <mhammad_fahs@hotmail.com>
2023-10-30 18:03:59 -0700

    Prime numbers recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,19 +1,6 @@
 #include "main.h"
 /**
-*is_prime_number - find prime numbers using recursion.
-*@n: Number to check if prime
-*
-*Return: 1 if Prime 0 Otherwise.
-*/
-int is_prime_number(int n)
-{
-if (n <= 1)
-return (0);
-else
-return (prime(n, n - 1));
-}
-/**
-*prime - Find the divisors of a number.
+*prime - Divide x over y.
 *
 *@x: Numerator.
 *@y: Denominator.
@@ -28,4 +15,17 @@ else if (x % y == 0)
 return (0);
 else
 return (prime(x, y - 1));
+}
+/**
+*is_prime_number - find prime numbers using recursion.
+*@n: Number to check if prime
+*
+*Return: 1 if Prime 0 Otherwise.
+*/
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+else
+return (prime(n, n - 1));
 }
