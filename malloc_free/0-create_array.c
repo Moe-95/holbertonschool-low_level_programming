bffdd9380a5e658e73ccae7163d3e40514f1cbf5
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-08 17:08:08 -0800

    Fix 3

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,9 +12,9 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i = 0;
+char *a = malloc(sizeof(c) * size);
 if (size == 0)
 return (NULL);
-char *a = malloc(sizeof(c) * size);
 while (i < size)
 {
 a[i] = c;
