52a58a0ec80249cfe94c5f8935877a279a1a3c41
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-08 16:55:01 -0800

    Malloc Array pointer

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "main.h"
+/**
+*create_array - create an array of size size.
+*
+*@c: Array char.
+*@size: Array size.
+*
+*Return: Array a.
+*/
+char *create_array(unsigned int size, char c)
+{
+int i = 0;
+char *a = malloc(sizeof(char) * size);
+if (size == 0)
+return (NULL);
+while (i < size)
+{
+a[i] = c;
+i++;
+}
+return (a);
+}
