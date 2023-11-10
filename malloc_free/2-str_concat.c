fd68abf8aadc1f8e9ba7b6f95b6b979a9110955a
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-10 05:26:17 -0800

    Fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,11 +13,13 @@ char *str_concat(char *s1, char *s2)
 {
 int i = 0, j = 0, q = 0, l = 0;
 char *a;
-while (s1[i] != '\0')
+if (s1 == NULL && s2 == NULL)
+return (NULL);
+while (s1 != NULL && s1[i] != '\0')
 {
 i++;
 }
-while (s2[j] != '\0')
+while (s2 != NULL && s2[j] != '\0')
 {
 j++;
 }
@@ -28,17 +30,13 @@ return (NULL);
 l = i;
 j = 0;
 i = 0;
-while (s1[i] != '\0')
+while (s1 != NULL && s1[i] != '\0')
 {
-if (a == NULL)
-return (NULL);
 a[i] = s1[i];
 i++;
 }
-while (s2[j] != '\0')
+while (s2 != NULL && s2[j] != '\0')
 {
-if (a == NULL)
-return (NULL);
 a[l] = s2[j];
 j++;
 l++;
