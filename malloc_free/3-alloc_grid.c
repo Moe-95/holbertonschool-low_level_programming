0c73dfbcaae9edbe83e9cb4aea8f54fb532c572b
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-10 08:15:18 -0800

    2 Dimensional array

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "main.h"
+/**
+*alloc_grid - return a 2 dimensional array.
+*
+*@width: width array.
+*@height: height array.
+*
+*Return: 2 dimensional Array grid.
+*/
+int **alloc_grid(int width, int height)
+{
+int i = 0, j = 0;
+int **grid;
+if (width <= 0 || height <= 0)
+return (NULL);
+grid = malloc(sizeof(int) * height);
+if (grid == NULL)
+return (NULL);
+while (i < height)
+{
+grid[i] = malloc(sizeof(int) * width);
+i++;
+}
+while (j < height)
+{
+i = 0;
+while (i < width)
+{
+grid[j][i] = 0;
+i++;
+}
+j++;
+}
+return (grid);
+}
