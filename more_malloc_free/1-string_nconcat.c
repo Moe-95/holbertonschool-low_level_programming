0e5631d89b926d7623b2f55dd7e8a251c69bfe6a
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-12 04:14:46 -0800

    Fix1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -28,11 +28,21 @@ i = 0;
 while (s1[i] != '\0')
 {
 a[i] = s1[i];
+if (a[i] == NULL)
+{
+free(a);
+return (NULL);
+}
 i++;
 }
 while (s2[j] != '\0' && j < n)
 {
 a[i] = s2[j];
+if (a[i] == NULL)
+{
+free(a);
+return (NULL);
+}
 i++;
 j++;
 }
