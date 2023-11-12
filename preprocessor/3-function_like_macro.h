1108f89ddd9db3ad1ebc800ba1e9b363f577d680
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-12 09:06:55 -0800

    Fix 5

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,6 +1,12 @@
-#ifndef _3_function_like_macro
-#define _3_function_like_macro
+
+#ifndef FUNCTION_LIKE_MACRO_H_3
+
+#define FUNCTION_LIKE_MACRO_H_3
+
+
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
-#endif
+
+
+#endif 
