f78c9d592f9d518e13ab4fa69ff7406a8bac1ada
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-17 08:29:30 -0800

    Last fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+*struct dog -dog data.
+*
+*@name: dog name.
+*@owner: dog owner.
+*@age: dog age.
+*Description: Define data of a variable of type dog.
+*/
 struct dog
 {
- char *name;
- char *owner;
- float age;
+char *name;
+char *owner;
+float age;
 };
 #endif
