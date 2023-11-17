18dc2eae59f90af6909bad2945da15d6851e08c3
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-17 07:46:46 -0800

    Initialize a variable of type dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*init_dog - initialize a variable of typr struct dog.
+*
+*@d: dog passed.
+*@name: dog name.
+*@age: dog age.
+*@owner: dog owner.
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
