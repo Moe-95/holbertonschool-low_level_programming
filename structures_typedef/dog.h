218903622c230954232637643288a57074eb2c26
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-18 10:56:48 -0800

    typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
 *@age: dog age.
 *Description: Define data of a variable of type dog.
 */
-struct dog
+typedef struct dog
 {
 char *name;
 char *owner;
 float age;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
