9994c4e493c4ce087402b28e069de3d3d210c446
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-19 05:54:40 -0800

    New dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"dog.h"
+/**
+*new_dog - create a new variable of type dog.
+*
+*@name: dog name.
+*@age: dog age.
+*@owner: dog owner.
+*
+*Return: dog p.
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *p;
+p = malloc(sizeof(dog_t));
+if (p == NULL)
+return (NULL);
+if (name == NULL)
+{
+free(p);
+free(owner);
+return (NULL);
+}
+if (owner == NULL)
+{
+free(p);
+free(namer);
+return (NULL);
+}
+p->name = name;
+p->age = age;
+p->owner = owner;
+return (p);
+}
