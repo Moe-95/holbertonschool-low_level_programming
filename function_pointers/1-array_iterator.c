d964f4afb8e904c86ff110efc43eac44995e42f8
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-19 07:27:55 -0800

    Iterates

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* array_iterator - performs an action on a whole array
+*
+* @array: array to act on
+* @size: size of array
+* @action: function to execute on array
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (action != NULL && array != NULL)
+while (i < size)
+{
+action(array[i]);
+i++;
+}
+}
