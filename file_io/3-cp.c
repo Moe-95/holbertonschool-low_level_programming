086a0e1c8abdcbf39d847b6bba7df72792340751
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-10 19:55:35 +0200

    2

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,100 +4,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * print_usage_error - prints usage error message and exits with code 97
+ * main - entry point
+ * @ac: argument count
+ * @av: array of argument tokens
+ * Return: 0 on success
  */
-void print_usage_error(void)
+int main(int ac, char *av[])
 {
-dprintf(2, "Usage: cp file_from file_to\n");
-exit(97);
-}
+int fd_from, fd_to, rd_stat, wr_stat;
+mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+char buffer[BUFSIZE];
 
-/**
- * print_read_error - prints read error message and exits with code 98
- * @file: the file name
- */
-void print_read_error(const char *file)
-{
-dprintf(2, "Error: Can't read from file %s\n", file);
-exit(98);
-}
-
-/**
- * print_write_error - prints write error message and exits with code 99
- * @file: the file name
- */
-void print_write_error(const char *file)
-{
-dprintf(2, "Error: Can't write to %s\n", file);
-}
-
-/**
- * close_files - closes file descriptors and prints error message if any
- * @fd1: the first file descriptor
- * @fd2: the second file descriptor
- * Return: 0 on success, -1 on failure
- */
-int close_files(int fd1, int fd2)
-{
-int result1, result2;
-
-result1 = close(fd1);
-result2 = close(fd2);
-
-if (result1 == -1 || result2 == -1)
-{
-if (result1 == -1)
-dprintf(2, "Error: Can't close fd %d\n", fd1);
-if (result2 == -1)
-dprintf(2, "Error: Can't close fd %d\n", fd2);
-return (-1);
-}
-
-return (0);
-}
-
-/**
- * main - entry point, copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, otherwise exit status
- */
-int main(int argc, char *argv[])
-{
-int fd_from, fd_to, bytes_read, bytes_written;
-char buffer[1024];
-if (argc != 3)
-print_usage_error();
-fd_from = open(argv[1], O_RDONLY);
+if (ac != 3)
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+fd_from = open(av[1], O_RDONLY);
 if (fd_from == -1)
-print_read_error(argv[1]);
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 if (fd_to == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+rd_stat = 1;
+while (rd_stat)
 {
-print_write_error(argv[2]);
-close(fd_from);
-exit(99);
-}
-do {
-bytes_read = read(fd_from, buffer, 1024);
-if (bytes_read == -1)
+rd_stat = read(fd_from, buffer, BUFSIZE);
+if (rd_stat == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+if (rd_stat > 0)
 {
-print_read_error(argv[1]);
-close(fd_from);
-close(fd_to);
-exit(98);
+wr_stat = write(fd_to, buffer, rd_stat);
+if (wr_stat != rd_stat || wr_stat == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 }
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-print_write_error(argv[2]);
-close(fd_from);
-close(fd_to);
-exit(99);
 }
-} while (bytes_read == 1024);
-if (close_files(fd_from, fd_to) == -1)
-exit(100);
+if (close(fd_from) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+if (close(fd_to) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 return (0);
 }
