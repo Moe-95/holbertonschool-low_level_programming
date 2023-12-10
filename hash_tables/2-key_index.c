5627ff5de3125ab5a5ed466ad6f1a9dd89898df6
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-10 20:22:16 +0200

    fix

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -6,10 +6,10 @@
  *
  * Return: The index at which the key should be stored in the array
  */
-unsigned long int key_index(const unsigned char*key,unsigned long int size)
+unsigned long int key_index(const unsigned char *key , unsigned long int size)
 {
-unsigned long int hash_value=hash_djb2(key);
-unsigned long int index=hash_value%size;
+unsigned long int hash_value = hash_djb2(key);
+unsigned long int index = hash_value % size;
 
-return(index);
+return (index);
 }
