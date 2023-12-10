64dcb8c15f1c4d9b775a1b6c1302648b382adefd
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-10 17:22:39 +0200

    fix

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,38 +4,45 @@
 #include <stdlib.h>
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters to read and print
+ * @filename: the name of the file
+ * @letters: the number of letters it should read and print
  *
- * Return: the actual number of letters it could read and print,
- * 0 if the file can't be opened or read, or if filename is NULL,
- * or if write fails or does not write the expected amount of bytes
+ * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+int file_descriptor;
+char *buffer;
+ssize_t bytes_read, bytes_written;
+
 if (filename == NULL)
 return (0);
 
-int file_descriptor = open(filename, O_RDONLY);
-
+file_descriptor = open(filename, O_RDONLY);
 if (file_descriptor == -1)
 return (0);
 
-char *buffer = malloc(sizeof(char) * letters);
-
+buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 {
 close(file_descriptor);
 return (0);
 }
 
-ssize_t bytes_read = read(file_descriptor, buffer, letters);
-ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+bytes_read = read(file_descriptor, buffer, letters);
+if (bytes_read == -1)
+{
+free(buffer);
+close(file_descriptor);
+return (0);
+}
+
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 free(buffer);
 close(file_descriptor);
 
-if (bytes_read == -1 || bytes_written == -1 || bytes_read != bytes_written)
+if (bytes_written != bytes_read)
 return (0);
 
 return (bytes_read);
