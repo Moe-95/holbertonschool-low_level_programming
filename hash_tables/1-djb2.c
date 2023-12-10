14551a6becf9cd2291ee233ef3e3d49787a3424b
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-10 20:19:43 +0200

    2

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -10,6 +10,6 @@ unsigned long int hash_djb2(const unsigned char *str)
 unsigned long int hash = 5381;
 int c;
 while ((c = *str++))
-hash = ((hash << 5) +hash) +c;
+hash = ((hash << 5) + hash) +c;
 return (hash);
 }
