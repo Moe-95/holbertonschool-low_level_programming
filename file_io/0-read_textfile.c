e2f65643037c89f85bea7aa2e00300160bf24837
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-10 17:12:13 +0200

    first TRY

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,7 +2,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: the actual number of letters it could read and print,
+ * 0 if the file can't be opened or read, or if filename is NULL,
+ * or if write fails or does not write the expected amount of bytes
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 if (filename == NULL)
