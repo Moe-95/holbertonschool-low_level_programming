25b5a516d83363ed35d945e846d48e1d58d185b2
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-10 17:52:59 +0200

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * create_file - creates a file and writes text content to it
+ * @filename: the name of the file to create
+ * @text_content: a NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int file_descriptor, bytes_written;
+mode_t permissions = S_IRUSR | S_IWUSR;  // rw-------
+
+if (filename == NULL)
+return (-1);
+
+file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permissions);
+if (file_descriptor == -1)
+return (-1);
+
+if (text_content != NULL)
+{
+bytes_written = write(file_descriptor, text_content, strlen(text_content));
+if (bytes_written == -1)
+{
+close(file_descriptor);
+return (-1);
+}
+}
+
+close(file_descriptor);
+return (1);
+}
