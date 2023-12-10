6e457fb5d276ef65c46f51c9f73730c1cd3155ac
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-10 20:13:23 +0200

    implement

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,15 @@
+#include"hash_tables.h"
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm
+ * @str: The input string
+ *
+ * Return: The hash value for the given input string
+ */
+unsigned long int hash_djb2(const unsigned char*str)
+{
+unsigned long int hash=5381;
+int c;
+while((c=*str++))
+hash=((hash<<5)+hash)+c;
+return(hash);
+}
