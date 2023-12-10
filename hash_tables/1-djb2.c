376aab5e3c7f525697c5f2a358267c1d093d9b0d
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-10 20:15:45 +0200

    1

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -5,11 +5,11 @@
  *
  * Return: The hash value for the given input string
  */
-unsigned long int hash_djb2(const unsigned char*str)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash=5381;
+unsigned long int hash = 5381;
 int c;
-while((c=*str++))
-hash=((hash<<5)+hash)+c;
+while ((c = *str++))
+hash = ((hash << 5) + hash) + c;
 return(hash);
 }
